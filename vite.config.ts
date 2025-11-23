5f9a90fb17ae01aee054c4ae313610be2dd4f53b
ronikiko <roni.k@finasium.com>
2025-11-23 02:33:50 +0200

    s

--- vite.config.ts
+++ vite.config.ts
@@ -3,13 +3,8 @@ import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig(({ mode }) => {
-    const env = loadEnv(mode, '.', '');
     return {
       base: '/mitch-store/',
-      server: {
-        port: 3000,
-        host: '0.0.0.0',
-      },
       plugins: [react()],
       resolve: {
         alias: {
